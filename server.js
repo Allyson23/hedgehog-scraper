eeba4c765eb063b0027589fd6ffa3157d02bad22
Allyson <Allyprincess6825@gmail.com>
2018-12-02 12:02:27 -0700

    Update server.js

--- server.js
+++ server.js
@@ -14,8 +14,46 @@ app.get('/', (req, res) => {
 });
 
 // your endpoint
+app.get('/pets',(req, res)=> {
+res.send("My dog is amazing!");
+});
 
 // scraper endpoint
+app.get('/hedgie/:keyword' , (req, res) => {
+  var keyword = req.params.keyword;
+  function findHedgieImage(keyword) {
+    var nightmare = Nightmare();
+
+return nightmare
+  .goto('https://www.google.com')
+  .insert('input[title="Search"]', `hedgehog ${keyword}`)
+  .click('input[value="Google Search"]')
+  .wait('a.q.qs')
+  .click('a.q.qs')
+  .wait('div#res.med')
+ .evaluate(function() {
+    var photoDivs = document.querySelectorAll('img.rg_ic');
+    var list = [].slice.call(photoDivs);
+
+    return list.map(function(div) {
+      return div.src;
+    });
+  })
+  .end()
+  .then(function(result) {
+    return result.slice(1, 5);
+  })
+  .then(function (images) {
+    res.json(images);
+  })
+   .catch(function(error) {
+    console.error('Search failed:' , error);
+  });
+}
+findHedgieImage(keyword);
+});
+
+
 
 app.listen(port, () => {
   console.log(`app running on ${port}`);
